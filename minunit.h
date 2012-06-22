f11a913519e9fa420266c45fb651b987f5f36371
David Siñuela Pastor <siu.4coders@gmail.com>
2012-06-22 13:03:51 +0200

    Make TEST_SUITE define main

--- minunit.h
+++ minunit.h
@@ -3,16 +3,26 @@ int minunit_assert = 0;
 int minunit_fail = 0;
 
 #define MU_TEST(method_name) static char * method_name()
-#define MU_TEST_SUITE(suite_name) static char * suite_name()
+#define MU_TEST_SUITE(suite_name) int main(int argc, char **argv)
 
-#define mu_assert(test, message) do { minunit_assert++; if (!(test)) { minunit_fail++; printf("F"); return "ERROR: " message; } else { printf("."); } } while (0)
-#define mu_run_test(test) do { char *message = test(); minunit_run++; \
-                               if (message) return message; } while (0)
+#define mu_assert(test, message) do {\
+		minunit_assert++;\
+		if (!(test)) {\
+			minunit_fail++;\
+			printf("F");\
+			return "ERROR: " message;\
+		} else {\
+			printf(".");\
+		}\
+	} while (0)
+
+#define mu_run_test(test) do {\
+		char *message = test();\
+		minunit_run++;\
+		if (message) {\
+			printf("\n%s\n", message);\
+		}\
+	} while (0)
 
 // 1 tests, 1 assertions, 0 failures, 0 errors
-#define mu_report() printf("\n%d tests, %d assertions, %d failures\n", minunit_run, minunit_assert, minunit_fail)
-
-
-//extern int minunit_run;
-//extern int minunit_fail;
-//extern int minunit_assert;
+#define mu_report() printf("\n\n%d tests, %d assertions, %d failures\n", minunit_run, minunit_assert, minunit_fail)
--- minunit_example.c
+++ minunit_example.c
@@ -23,12 +23,6 @@ MU_TEST_SUITE(all_tests) {
     mu_run_test(test_bar);
     mu_run_test(test_foo);
     mu_run_test(test_foo);
-    return 0;
-}
-
-int main(int argc, char **argv) {
-    char *result = all_tests();
 	mu_report();
-
-    return result != 0;
+    return 0;
 }
