aa5b0c076458b8ac9b90255f21a0e6753a4d5915
David Siñuela Pastor <siu.4coders@gmail.com>
2012-06-25 14:55:36 +0200

    Add setup/teardown functions

--- minunit.h
+++ minunit.h
@@ -1,27 +1,42 @@
+#include <malloc.h>
+#include <string.h>
+
 static int minunit_run = 0;
 static int minunit_assert = 0;
 static int minunit_fail = 0;
 
+void (*mu_setup)(void) = NULL;
+void (*mu_teardown)(void) = NULL;
+
 #define MU_TEST(method_name) static char * method_name()
-#define MU_TEST_SUITE(suite_name) int main(int argc, char **argv)
+#define MU_TEST_SUITE(suite_name) void suite_name()
+
+#define MU_SUITE_CONFIGURE(setup_fun, teardown_fun) \
+	mu_setup = setup_fun; \
+	mu_teardown = teardown_fun;
 
 #define mu_assert(test, message) do {\
 		minunit_assert++;\
 		if (!(test)) {\
 			minunit_fail++;\
 			printf("F");\
-			return "ERROR: " message;\
+			char *m = (char *) malloc(strlen(message) + 30);\
+			sprintf(m, "%s failed:\n\t%s:%d: %s", __func__, __FILE__, __LINE__, message);\
+			return m;\
 		} else {\
 			printf(".");\
 		}\
 	} while (0)
 
 #define mu_run_test(test) do {\
+		if (mu_setup) (*mu_setup)();\
 		char *message = test();\
 		minunit_run++;\
 		if (message) {\
 			printf("\n%s\n", message);\
 		}\
+		fflush(stdout);\
+		if (mu_teardown) (*mu_teardown)();\
 	} while (0)
 
 // 1 tests, 1 assertions, 0 failures, 0 errors
--- minunit_example.c
+++ minunit_example.c
@@ -1,28 +1,46 @@
 #include <stdio.h>
 #include "minunit.h"
 
-int foo = 7;
-int bar = 4;
+static int foo = 0;
+static int bar = 0;
+
+void test_setup() {
+	foo = 7;
+	bar = 4;
+}
+
+/*
+void test_teardown() {
+}
+*/
 
 MU_TEST(test_foo) {
-    mu_assert(foo == 7, "foo should be 7");
-    return 0;
+	mu_assert(foo == 7, "foo should be 7");
+	return 0;
 }
 
 MU_TEST(test_bar) {
-    mu_assert(bar == 5, "bar should be 5");
-    return 0;
+	mu_assert(bar == 5, "bar should be 5");
+	return 0;
 }
 
-MU_TEST_SUITE(all_tests) {
-    mu_run_test(test_foo);
-    mu_run_test(test_foo);
-    mu_run_test(test_foo);
-    mu_run_test(test_foo);
-    mu_run_test(test_foo);
-    mu_run_test(test_bar);
-    mu_run_test(test_foo);
-    mu_run_test(test_foo);
-	mu_report();
-    return 0;
+MU_TEST_SUITE(test_suite) {
+	MU_SUITE_CONFIGURE(&test_setup, NULL);
+	//MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
+
+	mu_run_test(test_foo);
+	mu_run_test(test_foo);
+	mu_run_test(test_foo);
+	mu_run_test(test_foo);
+	mu_run_test(test_foo);
+	mu_run_test(test_bar);
+	mu_run_test(test_foo);
+	mu_run_test(test_foo);
 }
+
+int main(int argc, char *argv[]) {
+	test_suite();
+	mu_report();
+	return 0;
+}
+
