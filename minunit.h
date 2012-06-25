e38763045b6670c55bbda83a5bc6fae75935f0af
David Siñuela Pastor <siu.4coders@gmail.com>
2012-06-25 18:06:36 +0200

    "namespace" and capitalize all macros

--- minunit.h
+++ minunit.h
@@ -5,17 +5,17 @@ static int minunit_run = 0;
 static int minunit_assert = 0;
 static int minunit_fail = 0;
 
-void (*mu_setup)(void) = NULL;
-void (*mu_teardown)(void) = NULL;
+void (*minunit_setup)(void) = NULL;
+void (*minunit_teardown)(void) = NULL;
 
 #define MU_TEST(method_name) static char * method_name()
 #define MU_TEST_SUITE(suite_name) void suite_name()
 
 #define MU_SUITE_CONFIGURE(setup_fun, teardown_fun) \
-	mu_setup = setup_fun; \
-	mu_teardown = teardown_fun;
+	minunit_setup = setup_fun; \
+	minunit_teardown = teardown_fun;
 
-#define mu_assert(test, message) do {\
+#define MU_ASSERT(test, message) do {\
 		minunit_assert++;\
 		if (!(test)) {\
 			minunit_fail++;\
@@ -28,16 +28,16 @@ void (*mu_teardown)(void) = NULL;
 		}\
 	} while (0)
 
-#define mu_run_test(test) do {\
-		if (mu_setup) (*mu_setup)();\
+#define MU_RUN_TEST(test) do {\
+		if (minunit_setup) (*minunit_setup)();\
 		char *message = test();\
 		minunit_run++;\
 		if (message) {\
 			printf("\n%s\n", message);\
 		}\
 		fflush(stdout);\
-		if (mu_teardown) (*mu_teardown)();\
+		if (minunit_teardown) (*minunit_teardown)();\
 	} while (0)
 
 // 1 tests, 1 assertions, 0 failures, 0 errors
-#define mu_report() printf("\n\n%d tests, %d assertions, %d failures\n", minunit_run, minunit_assert, minunit_fail)
+#define MU_REPORT() printf("\n\n%d tests, %d assertions, %d failures\n", minunit_run, minunit_assert, minunit_fail)
--- minunit_example.c
+++ minunit_example.c
@@ -15,12 +15,12 @@ void test_teardown() {
 */
 
 MU_TEST(test_foo) {
-	mu_assert(foo == 7, "foo should be 7");
+	MU_ASSERT(foo == 7, "foo should be 7");
 	return 0;
 }
 
 MU_TEST(test_bar) {
-	mu_assert(bar == 5, "bar should be 5");
+	MU_ASSERT(bar == 5, "bar should be 5");
 	return 0;
 }
 
@@ -28,19 +28,19 @@ MU_TEST_SUITE(test_suite) {
 	MU_SUITE_CONFIGURE(&test_setup, NULL);
 	//MU_SUITE_CONFIGURE(&test_setup, &test_teardown);
 
-	mu_run_test(test_foo);
-	mu_run_test(test_foo);
-	mu_run_test(test_foo);
-	mu_run_test(test_foo);
-	mu_run_test(test_foo);
-	mu_run_test(test_bar);
-	mu_run_test(test_foo);
-	mu_run_test(test_foo);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_bar);
+	MU_RUN_TEST(test_foo);
+	MU_RUN_TEST(test_foo);
 }
 
 int main(int argc, char *argv[]) {
 	test_suite();
-	mu_report();
+	MU_REPORT();
 	return 0;
 }
 
